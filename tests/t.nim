27e442d6cc8aadcd6caef1b89664fe88489c48d3
Will Taylor <w1ll100@hotmail.co.uk>
2021-08-19 22:58:14 +0100

    Some tests I forgot to add to git

---
+++ tests/t.nim
@@ -0,0 +1,37 @@
+import unittest
+import ddnimlib / linear
+import types
+
+suite "test Facing ops":
+  test "addition":
+    check:
+      North + East == East
+      North + South == South
+      North + West == West
+      East + North == East
+      East + West == North
+      West + West == South
+  
+  test "subtraction":
+    check:
+      North - East == West
+      North - West == East
+      North - North == North
+      East - East == North
+      West - East == South
+
+  test "identity rotation":
+    let v = vec(1, 1)
+    check:
+      v.rotate(North, 3) == vec(1, 1)
+      v.rotate(East, 3) == vec(1, 1)
+      v.rotate(South, 3) == vec(1, 1)
+      v.rotate(West, 3) == vec(1, 1)
+  
+  test "rotation":
+    let v = vec(1, 0)
+    check:
+      v.rotate(North, 5) == vec(1, 0)
+      v.rotate(East, 5) == vec(4, 1)
+      v.rotate(South, 5) == vec(3, 4)
+      v.rotate(West, 5) == vec(0, 3)
